e6322224584048a667d6c06a69b842836e62f785
shubhchaudhari7901 <shubhamchaudhari348917@gmail.com>
2025-10-31 12:50:08 +0530

    feat: add Spring Boot backend project with maven build

---
+++ backend/Inventory-Stock-Management-backend/src/main/java/com/Inventory_Stock_Management/Inventory_Stock_Management_backend/InventoryStockManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Inventory_Stock_Management.Inventory_Stock_Management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryStockManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryStockManagementBackendApplication.class, args);
+	}
+
+}
---
+++ backend/Inventory-Stock-Management-backend/src/test/java/com/Inventory_Stock_Management/Inventory_Stock_Management_backend/InventoryStockManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Inventory_Stock_Management.Inventory_Stock_Management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryStockManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
